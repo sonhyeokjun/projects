796666ec4896f7c386be46064d4e66b2e3cfc357
sonhyeokjun <107041119+sonhyeokjun@users.noreply.github.com>
2022-08-26 23:56:13 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+import requests
+from bs4 import BeautifulSoup
+
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.kfpqy18.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
@@ -9,28 +12,37 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
+@app.route("/movie", methods=["POST"])
+def movie_post():
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
 
-@app.route("/mars", methods=["POST"])
-def web_mars_post():
-    name_receive = request.form['name_give']
-    address_receive = request.form['address_give']
-    size_receive = request.form['size_give']
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    title = soup.select_one('meta[property="og:title"]')['content']
+    image = soup.select_one('meta[property="og:image"]')['content']
+    desc = soup.select_one('meta[property="og:description"]')['content']
 
     doc = {
-        'name':name_receive,
-        'address':address_receive,
-        'size':size_receive
+        'title':title,
+        'image':image,
+        'desc':desc,
+        'star':star_receive,
+        'comment':comment_receive
     }
-    db.mars.insert_one(doc)
+    db.movies.insert_one(doc)
 
+    return jsonify({'msg':'저장 완료!'})
 
-    return jsonify({'msg': '주문 완료!'})
-
-
-@app.route("/mars", methods=["GET"])
-def web_mars_get():
-    order_list = list(db.mars.find({}, {'_id': False}))
-    return jsonify({'orders':order_list})
+@app.route("/movie", methods=["GET"])
+def movie_get():
+    movie_list = list(db.movies.find({}, {'_id': False}))
+    return jsonify({'movies':movie_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
